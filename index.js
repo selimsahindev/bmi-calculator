880f5293a080a0a90ae11978ca42d7358282d658
Selim Şahin <selimsahin.sns@gmail.com>
2022-10-23 19:32:50 +0300

    reading and resetting gender radio buttons' checked status

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ form.addEventListener('submit', handleOnSubmit);
 const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener('click', handleOnClear);
 
-console.log('cookie readed:' + getCookie('firstName'));
+var genderRadios = document.getElementsByName('genderRadioOptions');
 
 function handleOnSubmit(event) {
   event.preventDefault();
@@ -14,14 +14,24 @@ function handleOnSubmit(event) {
     lastName: document.getElementById('lastNameInput').value,
     height: document.getElementById('heightInput').value,
     weight: document.getElementById('weightInput').value,
-    birthDate: document.getElementById('birthDateInput').value
+    birthDate: document.getElementById('birthDateInput').value,
+    gender: getGender()
   }
 
-  setCookie('firstName', formData.firstName, 2);
+  saveDataToCookie(formData);
 
-  console.log(formData);
+  window.location.replace(window.location.origin + '/index.html');
+}
 
-  //window.location.replace(window.location.origin + '/female.html');
+function saveDataToCookie(data) {
+  if (data == null && typeof data === 'undefined') {
+    return;
+  }
+
+  for (const [key, value] of Object.entries(data)) {
+    setCookie(key, value, 2);
+    //console.log(typeof key + ' ' + key + ', ' + typeof value + ' ' + value);
+  }
 }
 
 function handleOnClear(event) {
@@ -32,4 +42,19 @@ function handleOnClear(event) {
   document.getElementById('heightInput').value = null;
   document.getElementById('weightInput').value = null;
   document.getElementById('birthDateInput').value = null;
+  
+  console.log(genderRadios);
+
+  for (let i = 0; i < genderRadios.length; i++) {
+    genderRadios[i].checked = false;
+  }
+}
+
+function getGender() {
+  for (let i = 0; i < genderRadios.length; i++) {
+    if (genderRadios[i].checked) {
+      return genderRadios[i].id;
+    }
+  }
+  return null;
 }
